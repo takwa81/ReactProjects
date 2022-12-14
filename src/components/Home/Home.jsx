33936b3c443da79dd93749f6324634d2f4a0633e
Takwa-Mhd <takwasyr81@gmail.com>
2022-12-14 21:00:56 +0200

    hh

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,19 +2,33 @@ import React from "react";
 import { Col, Row } from "reactstrap";
 import Banner from "../Banners/Banner";
 import ProductList from "../Products/ProductList";
-import { motion } from "framer-motion";
-import ProductsApi from "../../Data/Api/Data";
+import CategoryList from "../Categories/CategoryList";
+import MenuMobile from "../Menu/MenuMobile";
+import Menu from "../Menu/Menu";
+import ProductsApi, { CategoriesApi } from "../../Data/Api/Data";
 import ProductShimmer from "../Loading/ProductShimmer";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
 
     const { products } = ProductsApi();
     const { loading } = ProductsApi();
+    const {categories} = CategoriesApi();
+    const [filter, setFilter] = useState(products);
+
 
     return (
-        <>
-            <Banner />
+
+        <div className="container-fluid p-2 h-100">
+        <Row className="h-100">
+            <Col md='2' className="">
+                <CategoryList categories={categories} />
+            </Col>
+            <Col md='10' className="main p-3">
+                <Menu />
+                <MenuMobile />
+                <Banner />
             <section className="popular-items mt-3">
                 <Row>
                     <Col md='6'>
@@ -28,7 +42,11 @@ const Home = () => {
                     loading ? <ProductShimmer></ProductShimmer>: <ProductList products={products}></ProductList>
                 }
             </section>
-        </>
+
+            </Col>
+        </Row>
+    </div>
+
     );
 
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import Router from "../../Router/Router";
 import '../styles/layout.css';
-import { Col, Row } from "reactstrap";
-import CategoryList from "../Categories/CategoryList";
-import Menu from "../Menu/Menu";
-import MenuMobile from "../Menu/MenuMobile";
-import { CategoriesApi } from "../../Data/Api/Data";
-import Loading from "../Loading/Loading";
+
 
 const Layout = () => {
-    const {categories} = CategoriesApi();
     const Style = {
         background: "linear-gradient(0deg, rgba(237,185,56,1) 0%, rgba(246,250,105,1) 52%)",
     }
@@ -19,18 +13,7 @@ const Layout = () => {
         <div className="gradient-container" style={Style}>
             <div className="content">
                 <div className="inner-container" id="scroll">
-                    <div className="container-fluid p-2 h-100">
-                        <Row className="h-100">
-                            <Col md='2' className="">
-                                <CategoryList categories={categories} />
-                            </Col>
-                            <Col md='10' className="main p-3">
-                                <Menu />
-                                <MenuMobile />
-                                <Router />
-                            </Col>
-                        </Row>
-                    </div>
+                  <Router />
                 </div>
             </div>
         </div>
